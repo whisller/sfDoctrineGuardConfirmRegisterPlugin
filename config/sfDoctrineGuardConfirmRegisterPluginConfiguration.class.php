0c7049084a50b96d35c08a02678f0f1b5358a972
Daniel Ancuta <whisller@gmail.com>
2011-01-07 16:57:17 +0100

    Fixed a typo in name of event "sf_guar_user.register_save_object"

--- config/sfDoctrineGuardConfirmRegisterPluginConfiguration.class.php
+++ config/sfDoctrineGuardConfirmRegisterPluginConfiguration.class.php
@@ -16,7 +16,7 @@ class sfDoctrineGuardConfirmRegisterPluginConfiguration extends sfPluginConfigur
         if (sfConfig::get('app_sf_guard_confirm_register_plugin_routes_register', true) && in_array('sfGuardConfirmRegister', sfConfig::get('sf_enabled_modules', array()))) {
             $this->dispatcher->connect('routing.load_configuration', array('sfGuardConfirmRegisterRouting', 'listenToRoutingLoadConfigurationEvent'));
 
-            $this->dispatcher->connect('sf_guar_user.register_save_object', array('sfGuardExtension', 'listenToUserRegister'));
+            $this->dispatcher->connect('sf_guard_user.register_save_object', array('sfGuardExtension', 'listenToUserRegister'));
         }
     }
 }
